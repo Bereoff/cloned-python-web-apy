cc7a52f150f270c46c72b77dbdfaa346c9646e31
Bruno Bereoff <bbereoff@gmail.com>
2023-01-02 20:39:46 -0300

    updated: black format

--- exemplos/day1/blog/wsgi.py
+++ exemplos/day1/blog/wsgi.py
@@ -35,7 +35,8 @@ def get_posts_from_database(post_id=None):
 
 def get_post_list(posts):
     post_list = [
-        f"""<li><a href="/{post['id']}">{post['title']}</a></li>""" for post in posts
+        f"""<li><a href="/{post['id']}">{post['title']}</a></li>"""
+        for post in posts
     ]
     return "\n".join(post_list)
 
@@ -50,7 +51,9 @@ def application(environ, start_reponse):
     # Roteamento de rotas/URLs
     if path == "/" and method == "GET":
         posts = get_posts_from_database()
-        body = render_template("list.template.html", post_list=get_post_list(posts))
+        body = render_template(
+            "list.template.html", post_list=get_post_list(posts)
+        )
         status = "200 OK"
 
     elif path.split("/")[-1].isdigit() and method == "GET":
